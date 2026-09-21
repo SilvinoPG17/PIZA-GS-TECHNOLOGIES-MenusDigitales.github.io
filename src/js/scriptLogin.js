4e2853deead6e0d0595476d1550b53754bfc44b5
SilvinoPG17 <spizag179604@gmail.com>
2026-09-21 13:17:21 -0600

    Se cambia ruta para servicio back

--- src/js/scriptLogin.js
+++ src/js/scriptLogin.js
@@ -1,6 +1,4 @@
-const API_URL = 'https://menusqr-febmaxevauewa3e5.mexicocentral-01.azurewebsites.net/api/usuarios/login';
-
-// const API_URL = 'https://localhost:7095/api/usuarios/login';
+const API_URL = 'http://pizagst-001-site1.itempurl.com/api/usuarios/login';
 
         document.getElementById('formLogin').addEventListener('submit', async (e) => {
             e.preventDefault();
--- src/js/scriptMenu.js
+++ src/js/scriptMenu.js
@@ -12,8 +12,7 @@
         }
 
         // URL base de tu API .NET (asegúrate de que coincida con tu puerto actual)
-        const API_BASE = 'https://menusqr-febmaxevauewa3e5.mexicocentral-01.azurewebsites.net/api/menus';
-        //  const API_BASE = 'https://localhost:7095/api/menus';
+        const API_BASE = 'http://pizagst-001-site1.itempurl.com/api/menus';
 
         // 1. EVENTO: NUEVO REGISTRO (Conecta con POST /api/menus/subir)
         document.getElementById('formNuevo').addEventListener('submit', async (e) => {
